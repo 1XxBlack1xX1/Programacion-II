eb1ce33c33fcf4e4e66d37c3418075cdb6b078a6
Sergio Marquina <danielquispe2512@gmail.com>
2025-04-08 10:55:17 -0500

    Add files via upload

---
+++ CalcFX/src/main/java/pe/edu/upeu/calcfx/Application.java
@@ -0,0 +1,12 @@
+package pe.edu.upeu.calcfx;
+
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        CalcFxApplication.main(args);
+
+
+    }
+}
